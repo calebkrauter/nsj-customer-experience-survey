bda7d9139b56dcdabf3b1e8edb70d2347794332f
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2026-02-07 10:46:42 -0700

    chore: touchups

--- app/api/post-user/route.ts
+++ app/api/post-user/route.ts
@@ -1,5 +1,6 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server';
+import { CookieKeys, CookieValues } from '../util';
 
 export const dynamic = 'force-dynamic'
 const passwords = [process.env.ADMIN_USER_PASSWORD, process.env.TEST_USER_PASSWORD, process.env.INTEGRATION_TEST_PASSWORD, process.env.EMPLOYEE_USER_PASSWORD]
@@ -9,16 +10,16 @@ export async function POST(request: Request) {
       for (const password of passwords) {
         const authenticated = (data.password === password )
         if (authenticated && data.password === process.env.EMPLOYEE_USER_PASSWORD) {
-          cookieStore.set('logged_in', 'employee', {maxAge: 30 * 24 * 60 * 60})
+          cookieStore.set(CookieKeys.LOGGED_IN, CookieValues.EMPLOYEE, {maxAge: 30 * 24 * 60 * 60})
           return Response.json({authenticated: authenticated});
         } else if (authenticated && data.password === process.env.ADMIN_USER_PASSWORD) {
-          cookieStore.set('logged_in', 'admin', {maxAge: 30 * 24 * 60})
+          cookieStore.set(CookieKeys.LOGGED_IN, CookieValues.ADMIN, {maxAge: 30 * 24 * 60})
           return Response.json({authenticated: authenticated});
         } else if (authenticated && data.password === process.env.INTEGRATION_TEST_PASSWORD) {
-          cookieStore.set('logged_in', 'integration_test', {maxAge: 30 * 24 * 60})
+          cookieStore.set(CookieKeys.LOGGED_IN, CookieValues.INTEGRATION_TEST, {maxAge: 30 * 24 * 60})
           return Response.json({authenticated: authenticated});
         } else if (authenticated && data.password === process.env.TEST_USER_PASSWORD) {
-          cookieStore.set('logged_in', 'test_user', {maxAge: 30 * 24 * 60})
+          cookieStore.set(CookieKeys.LOGGED_IN, CookieValues.TEST_USER, {maxAge: 30 * 24 * 60})
           return Response.json({authenticated: authenticated});
         }
       }
--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,29 +1,30 @@
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server';
 import { db } from '../database/db';
+import { CookieKeys, CookieValues, SurveyKey } from '../util';
 
 
 export const dynamic = 'force-dynamic'
 
 export async function POST(request: Request) {
   const data = await request.json();
-  const createdBy = (await cookies()).get('logged_in');
+  const createdBy = (await cookies()).get(CookieKeys.LOGGED_IN);
 
-  if (data.surveyKey === 'feedback') {
+  if (data.surveyKey === SurveyKey.FEEDBACK) {
     await db.query(`INSERT INTO "feedback_submissions"
         (surveyKey, createdBy, submissionId, rowVersion, creatorId, createdAt, modifiedBy, submissionData)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
         [data.surveyKey, createdBy?.value, data.submissionId, data.rowVersion, data.creatorId, data.createdAt, data.modifiedBy, JSON.stringify(data.submissionData)]
       )  } else {
 
-    if (createdBy?.value === 'test_user' || createdBy?.value === 'integration_test' || createdBy?.value === 'admin'){
+    if (createdBy?.value === CookieValues.TEST_USER || createdBy?.value === CookieValues.INTEGRATION_TEST || createdBy?.value === CookieValues.ADMIN){
       await db.query(
         `INSERT INTO "test_submissions"
         (surveyKey, createdBy, submissionId, rowVersion, creatorId, createdAt, modifiedBy, submissionData)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
         [data.surveyKey, createdBy?.value, data.submissionId, data.rowVersion, data.creatorId, data.createdAt, data.modifiedBy, JSON.stringify(data.submissionData)]
       )
-    } else if (createdBy?.value === 'employee') {
+    } else if (createdBy?.value === CookieValues.EMPLOYEE) {
           await db.query(
         `INSERT INTO "submissions"
         (surveyKey, createdBy, submissionId, rowVersion, creatorId, createdAt, modifiedBy, submissionData)
